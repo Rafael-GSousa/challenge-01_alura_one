70f81905cbe2a52943d8c67171527c6b454c73ce
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-12-26 00:52:09 -0300

    ajustando projeto

--- assets/js/decoder.js
+++ assets/js/decoder.js
@@ -1,72 +1,9 @@
 const encryptButton = document.getElementById('encrypt-button')
 const decryptButton = document.getElementById('decrypt-button')
-/*
-encryptButton.onclick = () => {
-  const inputText = document.getElementById('input-text').value
-  const output = document.getElementById('output')
-  const outputText = document.getElementById('output-text')
-  const hideImage = document.getElementById('hide-image')
-  const hideTitle = document.getElementById('hide-title')
-  const arrayListText = inputText.split('')
-  const arrayListEncrypt = []
-
-  for (let i = 0; i < arrayListText.length; i++) {
-    if (arrayListText[i] === 'a') {
-      arrayListEncrypt.push('ai')
-    } else if (arrayListText[i] === 'e') {
-      arrayListEncrypt.push('enter')
-    } else if (arrayListText[i] === 'i') {
-      arrayListEncrypt.push('imes')
-    } else if (arrayListText[i] === 'o') {
-      arrayListEncrypt.push('ober')
-    } else if (arrayListText[i] === 'u') {
-      arrayListEncrypt.push('ufat')
-    } else {
-      arrayListEncrypt.push(arrayListText[i])
-    }
-  }
-
-  hideImage.style.display = 'none'
-  hideTitle.style.display = 'none'
-  output.style.justifyContent = 'start'
-
-  outputText.innerHTML = arrayListEncrypt.join('')
-  output.innerHTML = '<textarea readonly id="output-text" class="section-output-in">' + outputText.innerHTML + 
-  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
- 
-}
-
-decryptButton.onclick = () => {
-  const inputText = document.getElementById('input-text').value
-  const output = document.getElementById('output')
-  const outputAreaText = document.getElementById('output-text')
-  const hideImage = document.getElementById('hide-image')
-  const hideTitle = document.getElementById('hide-title')
-
-  const decrypt = inputText.replace(/enter/g, 'e')
-  .replace(/imes/g, 'i')
-  .replace(/ai/g, 'a')
-  .replace(/ober/g, 'o')
-  .replace(/ufat/g, 'u')
-
-  hideImage.style.display = 'none'
-  hideTitle.style.display = 'none'
-  output.style.justifyContent = 'start'
-
-  outputAreaText.innerHTML = decrypt
-
-}
-
-function copiar() {
-    var textoCop = document.getElementById('output-text');
-  
-    textoCop.select();
-    document.execCommand('copy');
-    alert("Texto copiado.");
-}*/
-
+const output = document.getElementById('output')
+const hideImage = document.getElementById('hide-image')
+const hideTitle = document.getElementById('hide-title')
 var textInput = document.getElementById("input-text");
-var outInput = document.getElementById("output");
 
 
 encryptButton.onclick = () =>{
@@ -80,10 +17,13 @@ encryptButton.onclick = () =>{
   .replace(/u/g, "ufat")
 
   
-  document.getElementById('output').innerHTML = '<textarea readonly id="output-text">'
+  document.getElementById('output').innerHTML = '<textarea readonly id="output-text" class="output-saida">'
    + resultCripto + '</textarea>' + 
    '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
-}
+   hideImage.style.display = 'none'
+   hideTitle.style.display = 'none'
+   output.style.justifyContent = 'start'
+  }
 
 decryptButton.onclick = () =>{
 
@@ -95,10 +35,13 @@ decryptButton.onclick = () =>{
     .replace(/ober/g, "o")
     .replace(/ufat/g, "u");
   
-    document.getElementById('output').innerHTML = '<textarea readonly id="output-text">'
+    document.getElementById('output').innerHTML = '<textarea readonly id="output-text" class="output-saida">'
      + resultDescripto + '</textarea>' + 
      '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
-}
+     hideImage.style.display = 'none'
+     hideTitle.style.display = 'none'
+     output.style.justifyContent = 'start'
+    }
 
 function copiar() {
     var textoCop = document.getElementById('output-text');
